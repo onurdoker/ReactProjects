18a746c652f636d7d0bad87ba6e0e63c6fe1cd29
Onur DÖKER <odoker@msn.com>
2025-08-27 11:20:03 +0300

    Refactor: Update React Router and project links

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 import Home from "./assets/pages/Home.jsx";
 import Project1 from "./assets/pages/Project1.jsx";
 import Project2 from "./assets/pages/Project2.jsx";
+import Project3 from "./assets/pages/Project3.jsx";
+import Project4 from "./assets/pages/Project4.jsx";
 
 // import Project2 from "./pages/Project2";
 
 function App() {
   return (
-      <Router>
-        <div>
-
-          <Routes>
-            <Route path="/"
-                   element={<Home/>}/>
-            <Route path="/project1"
-                   element={<Project1/>}/>
-            <Route path="/project2"
-                   element={<Project2/>}/>
-          </Routes>
-        </div>
-      </Router>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project1" element={<Project1 />} />
+          <Route path="/project2" element={<Project2 />} />
+          <Route path="/project3" element={<Project3 />} />
+          <Route path="/project4" element={<Project4 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,16 +1,24 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-      <div>
-        <h1>Main Page</h1>
-        <ul>
-          <li><Link to="/project1">Project 1. Changing Font Size</Link></li>
-          <li><Link to="/project2">Project 2. Changing Background Color</Link></li>
-          <li><Link to="/project3">Project 3</Link></li>
-          <li><Link to="/project4">Project 4. Slider</Link></li>
-        </ul>
-      </div>
+    <div className="main">
+      <h1>Main Page</h1>
+      <ul>
+        <li>
+          <Link to="/project1">Project 1. Changing Font Size</Link>
+        </li>
+        <li>
+          <Link to="/project2">Project 2. Changing Text Color</Link>
+        </li>
+        <li>
+          <Link to="/project3">Project 3.</Link>
+        </li>
+        <li>
+          <Link to="/project4">Project 4. Slider</Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
